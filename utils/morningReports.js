0434ea637ddb6417e9f4e4ab912e924849e3b873
Dezeabasili <ezeabasili@yahoo.co.uk>
2024-03-15 16:42:17 -0500

    Create morningReports.js

--- utils/morningReports.js
+++ utils/morningReports.js
@@ -2,6 +2,15 @@ const sendOutMail = require("./handleEmail3");
 const db = require("./mysqlConnectionWithPromise");
 const schedule = require("node-schedule")
 
+// function to format date
+const formatDate = (value) => {
+    let date = new Date(value);
+    const day = date.toLocaleString('default', { day: '2-digit' });
+    const month = date.toLocaleString('default', { month: 'short' });
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    return day + '-' + month + '-' + year;
+  }
+
 const sendMorningReport = async () => {
     const mysqlConnection = await db();
 
