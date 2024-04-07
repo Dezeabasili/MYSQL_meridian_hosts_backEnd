af2032864b6cc4b33827ea05c972e76717cf5c3b
Dezeabasili <ezeabasili@yahoo.co.uk>
2024-04-07 14:13:41 -0500

    Update MySQL connection details

--- utils/mysqlConnectionWithPromise.js
+++ utils/mysqlConnectionWithPromise.js
@@ -18,7 +18,20 @@ const db =  async () => {
 
   // })
 
-  const conn = await mysql.createConnection(process.env.DATABASE_URL)
+  const conn = await mysql.createConnection({
+    host: process.env.AIVEN_HOST,
+    user: process.env.AIVEN_USER,
+    password: process.env.AIVEN_PASSWORD,
+    database: process.env.AIVEN_DATABASE,
+    port: process.env.AIVEN_PORT,
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync(path.join(__dirname, "..", "ca.pem")).toString(),
+    },
+
+  })
+
+  // const conn = await mysql.createConnection(process.env.DATABASE_URL)
 
       return conn
 }
